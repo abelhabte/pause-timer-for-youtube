1f35f8312b8408b647b1614d07b1c630f26b2fd5
abelhabte <132626177+abelhabte@users.noreply.github.com>
2026-05-13 21:46:05 -0400

    Condensed Overall Project
    Removed popup.html, popup.js
    
    Code from popup.js ported to background.js
    
    Added Scripting as a permission to manifest.json

--- background.js
+++ background.js
@@ -1,10 +1,19 @@
-// background.js
-chrome.action.onClicked.addListener((tab) => {
-  // Check if tab and tab.url exist before calling startsWith
-  if (
-    tab?.url?.startsWith("https://www.youtube.com/") ||
-    tab?.url?.startsWith("https://music.youtube.com/")
-  ) {
-    chrome.tabs.sendMessage(tab.id, { action: "togglePanel" });
+chrome.action.onClicked.addListener(async (tab) => {
+  if (tab?.url?.match(/https:\/\/(www|music)\.youtube\.com\/.*/)) {
+    try {
+      // 1. Try sending the message first
+      await chrome.tabs.sendMessage(tab.id, { action: "togglePanel" });
+    } catch (err) {
+      // 2. If it fails, the script isn't there. Inject it manually!
+      await chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        files: ["content.js"]
+      });
+      
+      // 3. Now that it's injected, send the message again
+      setTimeout(() => {
+        chrome.tabs.sendMessage(tab.id, { action: "togglePanel" });
+      }, 100); 
+    }
   }
 });
