c48b4010f1074f4431606d0a641401d7d50cb038
abelhabte <132626177+abelhabte@users.noreply.github.com>
2026-05-20 20:31:25 -0400

    Update background.js
    Updated background.js.

--- background.js
+++ background.js
@@ -1,19 +1,15 @@
 chrome.action.onClicked.addListener(async (tab) => {
   if (tab?.url?.match(/https:\/\/(www|music)\.youtube\.com\/.*/)) {
     try {
-      // 1. Try sending the message first
       await chrome.tabs.sendMessage(tab.id, { action: "togglePanel" });
     } catch (err) {
-      // 2. If it fails, the script isn't there. Inject it manually!
       await chrome.scripting.executeScript({
         target: { tabId: tab.id },
-        files: ["content.js"]
+        files: ["content.js"],
       });
-      
-      // 3. Now that it's injected, send the message again
       setTimeout(() => {
         chrome.tabs.sendMessage(tab.id, { action: "togglePanel" });
-      }, 100); 
+      }, 100);
     }
   }
 });
